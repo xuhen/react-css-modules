069ecf8f7833bff275242b480cd269a4a161d626
Edward xu <xuheng@goodsfans.com>
2018-03-28 17:01:54 +0800

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './App.css';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div className={style.title}>
+            Hello World!!!
+         </div>
+      );
+   }
+}
+export default App;
